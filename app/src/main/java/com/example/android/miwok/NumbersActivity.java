9577e51bc86f5549a4ce932f548c56ed1606cf47
Alex <akashlakhera05@gmail.com>
2017-06-22 00:18:43 +0530

    Added colors to specific activities Added placeholder text for all activities Added original images and text for Numbers Activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,28 +1,34 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.list_view);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("lutti","one",R.drawable.number_one));
+        words.add(new Word("otiiko","two",R.drawable.number_two));
+        words.add(new Word("tolookosu","three",R.drawable.number_three));
+        words.add(new Word("oyyisa","four",R.drawable.number_four));
+        words.add(new Word("massokka","five",R.drawable.number_five));
+        words.add(new Word("temmokka","six",R.drawable.number_six));
+        words.add(new Word("kenekaku","seven",R.drawable.number_seven));
+        words.add(new Word("kawinta","eight",R.drawable.number_eight));
+        words.add(new Word("wo’e","nine",R.drawable.number_nine));
+        words.add(new Word("na’aacha","ten",R.drawable.number_ten));
+
+        WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_numbers);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
