5114504f78d57aec9157f8e9bdc3d2db88019152
Alex <akashlakhera05@gmail.com>
2017-06-22 00:46:02 +0530

    Revert "Added colors to specific activities"
    No need for previous commit as the project is itself completed.
    This reverts commit 9577e51bc86f5549a4ce932f548c56ed1606cf47.

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,34 +1,28 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
-
-import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_view);
-
-        ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("lutti","one"));
-        words.add(new Word("otiiko","two"));
-        words.add(new Word("tolookosu","three"));
-        words.add(new Word("oyyisa","four"));
-        words.add(new Word("massokka","five"));
-        words.add(new Word("temmokka","six"));
-        words.add(new Word("kenekaku","seven"));
-        words.add(new Word("kawinta","eight"));
-        words.add(new Word("wo’e","nine"));
-        words.add(new Word("na’aacha","ten"));
-
-        WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_colors);
-
-        ListView listView = (ListView) findViewById(R.id.list);
-
-        listView.setAdapter(itemsAdapter);
+        setContentView(R.layout.activity_colors);
     }
 }
